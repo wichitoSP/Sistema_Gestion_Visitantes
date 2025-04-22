abd9f6ea660381b579bd77df525366d0b60b07de
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-04-22 17:41:03 -0600

    Creamos el proyecto

---
+++ src/main/java/com/mycompany/sitema_gestion_visitas_condominio_el_frutal2/Sitema_Gestion_Visitas_Condominio_El_Frutal2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sitema_gestion_visitas_condominio_el_frutal2;
+
+/**
+ *
+ * @author LuisAmaya
+ */
+public class Sitema_Gestion_Visitas_Condominio_El_Frutal2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
